da5fb6e6b1fdeb0cfc49e0303a074a7d10621d19
lukasalexandre <larosa2019@hotmail.com>
2024-09-24 06:28:07 -0300

    updating files

--- connectMongo.js
+++ connectMongo.js
@@ -5,7 +5,7 @@ mongoose.set("strictQuery", true)
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@server-imob.stodyvc.mongodb.net/${process.env.NAME_BASE_URL}?retryWrites=true&w=majority&appName=server-imob`);
+        await mongoose.connect(process.env.DATABASE_URL);
         console.log("Connect to MongoDB successfully");
     } catch (error) {
         console.log("Connect failed " + error.message );
